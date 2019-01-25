deeb8e261997b2ec10dd15be494b8707a6656a60
Kazimierz <kgofron@bnl.gov>
2019-01-25 13:56:25 -0500

    fix vendor script

--- LambdaSupport/vendor.sh
+++ LambdaSupport/vendor.sh
@@ -12,17 +12,17 @@ cd libfsdetcore-build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=../libfsdetcore-linux-x86_64 ../libfsdetcore
 make
 make install
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/epics/support/areaDetector-3-3/ADLambda/LambdaSupport/libfsdetcore-linux-x86_64/lib/pkgconfig
+export `PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/epics/support/areaDetector/ADLambda/LambdaSupport/libfsdetcore-linux-x86_64/lib/pkgconfig`
 cd ..
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/epics/support/areaDetector-3-3/ADLambda/LambdaSupport/libfsdetcore-linux-x86_64/lib
+export `LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/epics/support/areaDetector/ADLambda/LambdaSupport/libfsdetcore-linux-x86_64/lib`
 
 echo "Configure, make and install liblambda"
 cd liblambda-build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=../liblambda-linux-x86_64 ../liblambda
 make
 make install
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/epics/support/areaDetector-3-3/ADLambda/LambdaSupport/liblambda-linux-x86_64/lib/pkgconfig
+export `PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/epics/support/areaDetector/ADLambda/LambdaSupport/liblambda-linux-x86_64/lib/pkgconfig`
 cd ..
 
 echo "Should be finished"
